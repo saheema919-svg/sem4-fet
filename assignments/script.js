d7d15a4b1053a8666d0f7e1c57162530e633de81
saheema919-svg <saheema919@gmail.com>
2026-05-15 20:33:51 +0530

    Update script.js

--- assignments/script.js
+++ assignments/script.js
@@ -1,45 +1,29 @@
 let attempts = 0;
 
-function handlePassword(){
+function handlePassword() {
+  let passwordBox = document.getElementById("passwordBox");
+  let result = document.getElementById("result");
+  let button = document.getElementById("mainButton");
+  let userPassword = passwordBox.value.toLowerCase();
 
-    let passwordBox = document.getElementById("passwordBox");
-    let result = document.getElementById("result");
-    let button = document.getElementById("mainButton");
-
-    let userPassword = passwordBox.value.toLowerCase();
-
-    if(userPassword === "saheema"){
-
-        result.innerHTML =
-        "<span style='color:green;font-weight:bold;'>"
-        + "Login Successful<br><br>"
-        + "Student Name : Saheema<br>"
-        + "Department : Computer Science<br>"
-        + "Semester : 4th Semester<br>"
-        + "Status : Active"
-        + "</span>";
-
+  if (userPassword === "saheema") {
+    result.innerHTML =
+      "<span style='color:green;font-weight:bold;'>Login Successful<br><br>" +
+      "Student Name : Saheema<br>" +
+      "Department : Computer Science<br>" +
+      "Semester : 4th Semester<br>" +
+      "Status : Active</span>";
+  } else {
+    attempts++;
+    if (attempts >= 3) {
+      result.innerHTML =
+        "<span style='color:red;font-weight:bold;'>Access Blocked!</span>";
+      passwordBox.disabled = true;
+      button.disabled = true;
     } else {
-
-        attempts++;
-
-        if(attempts >= 3){
-
-            result.innerHTML =
-            "<span style='color:red;font-weight:bold;'>Access Blocked!</span>";
-
-            passwordBox.disabled = true;
-            button.disabled = true;
-
-        } else {
-
-            result.innerHTML =
-            "<span style='color:red;font-weight:bold;'>"
-            + "Wrong Username<br>"
-            + "Remaining Attempts : "
-            + (3 - attempts)
-            + "</span>";
-        }
+      result.innerHTML =
+        "<span style='color:red;font-weight:bold;'>Wrong Username<br>" +
+        "Remaining Attempts : " + (3 - attempts) + "</span>";
     }
+  }
 }
-</script>
