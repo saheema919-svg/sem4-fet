fd31763a590bea99d879dd164d6f7fa677cb99da
saheema919-svg <saheema919@gmail.com>
2026-05-15 20:40:06 +0530

    Update script.js

--- assignments/script.js
+++ assignments/script.js
@@ -1,29 +1,100 @@
+let savedPassword = "";
 let attempts = 0;
+let isPasswordCreated = false;
 
-function handlePassword() {
-  let passwordBox = document.getElementById("passwordBox");
-  let result = document.getElementById("result");
-  let button = document.getElementById("mainButton");
-  let userPassword = passwordBox.value.toLowerCase();
+function handlePassword(){
 
-  if (userPassword === "saheema") {
-    result.innerHTML =
-      "<span style='color:green;font-weight:bold;'>Login Successful<br><br>" +
-      "Student Name : Saheema<br>" +
-      "Department : Computer Science<br>" +
-      "Semester : 4th Semester<br>" +
-      "Status : Active</span>";
-  } else {
-    attempts++;
-    if (attempts >= 3) {
-      result.innerHTML =
-        "<span style='color:red;font-weight:bold;'>Access Blocked!</span>";
-      passwordBox.disabled = true;
-      button.disabled = true;
-    } else {
-      result.innerHTML =
-        "<span style='color:red;font-weight:bold;'>Wrong Username<br>" +
-        "Remaining Attempts : " + (3 - attempts) + "</span>";
+    let passwordBox =
+    document.getElementById("passwordBox");
+
+    let result =
+    document.getElementById("result");
+
+    let button =
+    document.getElementById("mainButton");
+
+    let label =
+    document.getElementById("labelText");
+
+    // Create Password
+    if(isPasswordCreated == false){
+
+        savedPassword = passwordBox.value;
+
+        if(savedPassword == ""){
+
+            result.innerHTML =
+            "<span style='color:red;'>"
+            + "Please create a password"
+            + "</span>";
+
+            return;
+        }
+
+        result.innerHTML =
+        "<span style='color:green;font-weight:bold;'>"
+        + "Password Created Successfully<br>"
+        + "Submit Password to Continue"
+        + "</span>";
+
+        passwordBox.value = "";
+
+        passwordBox.placeholder =
+        "Enter Password";
+
+        label.innerHTML =
+        "Enter Password";
+
+        button.innerHTML =
+        "Login";
+
+        isPasswordCreated = true;
+
+        return;
     }
-  }
+
+    // Verify Password
+    let userPassword = passwordBox.value;
+
+    // Do While Loop
+    do{
+
+        if(userPassword == savedPassword){
+
+            result.innerHTML =
+            "<span style='color:green;font-weight:bold;'>"
+            + "Correct Password<br><br>"
+            + "Student Name : saheema<br>"
+            + "Department : Computer Science<br>"
+            + "Semester : 4th Semester<br>"
+            + "Status : Active"
+            + "</span>";
+
+            return;
+        }
+
+        attempts++;
+
+        result.innerHTML =
+        "<span style='color:red;font-weight:bold;'>"
+        + "Wrong Password<br>"
+        + "Remaining Attempts : "
+        + (3 - attempts)
+        + "</span>";
+
+    }while(false);
+
+    // While Loop
+    while(attempts >= 3){
+
+        result.innerHTML =
+        "<span style='color:red;font-weight:bold;'>"
+        + "Access Blocked!"
+        + "</span>";
+
+        passwordBox.disabled = true;
+        button.disabled = true;
+
+        break;
+    }
 }
